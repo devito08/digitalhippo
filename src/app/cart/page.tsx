71b7fd9bebbe7be47c834c19481b5f5dfc3510e2
devito08 <dannydevito.cse@gmail.com>
2024-05-30 11:46:30 +0530

    stripe

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -19,6 +19,7 @@ const Page = () => {
   const { mutate: createCheckoutSession, isLoading } =
     trpc.payment.createSession.useMutation({
       onSuccess: ({ url }) => {
+        
         if (url) router.push(url)
       },
     })
