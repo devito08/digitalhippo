d63e26b3f9767d682b14be6a89769ced4e5140ab
danny <dannydevito.cse@gmail.com>
2024-07-04 19:49:59 +0530

    mogodb url changed

--- src/server.ts
+++ src/server.ts
@@ -1,64 +1,20 @@
-// import express from "express"
-// import { getPayloadClient } from "./get-payload"
-// import { nextApp, nextHandler } from "./next-utils"
-// import * as trpcExpress from "@trpc/server/adapters/express"
-// import { appRouter } from "./trpc"
-// import { inferAsyncReturnType } from "@trpc/server"
+import express from 'express';
+import cors from 'cors';
+import { getPayloadClient } from './get-payload';
+import { nextApp, nextHandler } from './next-utils';
+import * as trpcExpress from '@trpc/server/adapters/express';
+import { appRouter } from './trpc';
+import { inferAsyncReturnType } from '@trpc/server';
+import bodyParser from 'body-parser';
+import { IncomingMessage } from 'http';
+import { stripeWebhookHandler } from './webhooks';
+import nextBuild from 'next/dist/build';
+import path from 'path';
+import { PayloadRequest } from 'payload/types';
+import { parse } from 'url';
 
-// const app=express()
-// const PORT=Number(process.env.PORT) ||3000
-// const createContext=({
-//     req,
-//     res,
-//     }: trpcExpress.CreateExpressContextOptions) => ({
-//         req,
-//         res,
-//     })
-//     export type ExpressContext = inferAsyncReturnType<typeof createContext>
-// const start =async ()=>{
-//     const payload =await getPayloadClient({
-//         initOptions:{
-//             express : app,
-//             onInit: async (cms) =>{
-//                 cms.logger.info(`Admin URL ${cms.getAdminURL()} `)
-//             },
-//         },
-//     })
-//     app.use('/api/trpc',trpcExpress.createExpressMiddleware({
-//      router:appRouter,
-//      createContext
-//     }))
-// app.use((req,res)=>  nextHandler(req,res))
-
-// nextApp.prepare().then(()=>{
-//     payload.logger.info("Next.js started")
-
-//     app.listen(PORT,async()=>{
-//         payload.logger.info(
-//             `Next.js APP URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`)
-//     })
-// })
-// }
-
-// start()
-
-
-import express from 'express'
-import { getPayloadClient } from './get-payload'
-import { nextApp, nextHandler } from './next-utils'
-import * as trpcExpress from '@trpc/server/adapters/express'
-import { appRouter } from './trpc'
-import { inferAsyncReturnType } from '@trpc/server'
-import bodyParser from 'body-parser'
-import { IncomingMessage } from 'http'
-import { stripeWebhookHandler } from './webhooks'
-import nextBuild from 'next/dist/build'
-import path from 'path'
-import { PayloadRequest } from 'payload/types'
-import { parse } from 'url'
-
-const app = express()
-const PORT = Number(process.env.PORT) || 3000
+const app = express();
+const PORT = Number(process.env.PORT) || 3000;
 
 const createContext = ({
   req,
@@ -66,89 +22,92 @@ const createContext = ({
 }: trpcExpress.CreateExpressContextOptions) => ({
   req,
   res,
-})
+});
 
 export type ExpressContext = inferAsyncReturnType<
   typeof createContext
->
+>;
 
 export type WebhookRequest = IncomingMessage & {
-  rawBody: Buffer
-}
+  rawBody: Buffer;
+};
 
 const start = async () => {
   const webhookMiddleware = bodyParser.json({
     verify: (req: WebhookRequest, _, buffer) => {
-      req.rawBody = buffer
+      req.rawBody = buffer;
     },
-  })
+  });
+
+  // Apply CORS middleware globally
+  app.use(cors());
 
   app.post(
     '/api/webhooks/stripe',
     webhookMiddleware,
     stripeWebhookHandler
-  )
+  );
 
   const payload = await getPayloadClient({
     initOptions: {
       express: app,
       onInit: async (cms) => {
-        cms.logger.info(`Admin URL: ${cms.getAdminURL()}`)
+        cms.logger.info(`Admin URL: ${cms.getAdminURL()}`);
       },
     },
-  })
+  });
 
   if (process.env.NEXT_BUILD) {
     app.listen(PORT, async () => {
       payload.logger.info(
         'Next.js is building for production'
-      )
+      );
 
       // @ts-expect-error
-      await nextBuild(path.join(__dirname, '../'))
+      await nextBuild(path.join(__dirname, '../'));
 
-      process.exit()
-    })
+      process.exit();
+    });
 
-    return
+    return;
   }
 
-  const cartRouter = express.Router()
+  const cartRouter = express.Router();
 
-  cartRouter.use(payload.authenticate)
+  cartRouter.use(payload.authenticate);
 
   cartRouter.get('/', (req, res) => {
-    const request = req as PayloadRequest
+    const request = req as PayloadRequest;
 
     if (!request.user)
-      return res.redirect('/sign-in?origin=cart')
+      return res.redirect('/sign-in?origin=cart');
 
-    const parsedUrl = parse(req.url, true)
-    const { query } = parsedUrl
+    const parsedUrl = parse(req.url, true);
+    const { query } = parsedUrl;
 
-    return nextApp.render(req, res, '/cart', query)
-  })
+    return nextApp.render(req, res, '/cart', query);
+  });
 
-  app.use('/cart', cartRouter)
+  app.use('/cart', cartRouter);
   app.use(
     '/api/trpc',
     trpcExpress.createExpressMiddleware({
       router: appRouter,
       createContext,
     })
-  )
+  );
 
-  app.use((req, res) => nextHandler(req, res))
+  app.use((req, res) => nextHandler(req, res));
 
   nextApp.prepare().then(() => {
-    payload.logger.info('Next.js started')
+    payload.logger.info('Next.js started');
 
     app.listen(PORT, async () => {
       payload.logger.info(
         `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
-      )
-    })
-  })
-}
+      );
+    });
+  });
+};
 
-start()
+start();
